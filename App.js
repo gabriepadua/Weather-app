2ada4102708e3a45a738cb0a5943edd8109faecb
Gabriel Pádua <padua4699@gmail.com>
2024-03-03 23:41:10 -0300

    conseguindo extrair dados de outros endpoints e adicionando os campos de info

--- App.js
+++ App.js
@@ -1,66 +1,87 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Pressable, Image } from 'react-native';
-import sun from './img/sun.png';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Image,
+} from "react-native";
+import sun from "./img/sun.png";
 
 export default function App() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
-  const apiKey = '2QOVyWSNAEqBA3pza57LB5DIY07VMuPQ';
-  const baseUrl = 'http://dataservice.accuweather.com/';
+  const apiKey = "2QOVyWSNAEqBA3pza57LB5DIY07VMuPQ";
+  const baseUrl = "http://dataservice.accuweather.com/";
 
   const getFormattedDate = (dateString) => {
     const date = new Date(dateString);
     return `${date.getDate()}`; //if i wanna
   };
 
-
-
   const getClima = () => {
     // Reset weatherData to avoid previous data sticking around during the fetch
     setWeatherData(null);
-  
-    let locationKey;
-  
-    // Fetch data from the first endpoint
-    fetch(`${baseUrl}locations/v1/cities/search?q=${city}&apikey=${apiKey}&language=pt-br`)
-      .then(response => response.json())
-      .then(data => {
-        locationKey = data[0].Key;
-        console.log('First Endpoint Data:', data);
-        // Return a Promise to chain the next fetch
-        const { EnglishName } = data[0].EnglishName;
 
-        return fetch(`${baseUrl}currentconditions/v1/${locationKey}?apikey=${apiKey}`);
-      })
-      .then(response => response.json())
-      .then(data => {
-        currentConditionsData = data; // Assign data to currentConditionsData
-        console.log('Second Endpoint Data:', currentConditionsData);
-        // Update weatherData with data from the first endpoint
-        setWeatherData(currentConditionsData[0]);
+    let locationKey;
+    let locationData;
+    let currentConditionsData;
+
+    // Fetch data from the first endpoint
+    fetch(
+      `${baseUrl}locations/v1/cities/search?q=${city}&apikey=${apiKey}&language=pt-br`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        locationData = data[0];
+        locationKey = locationData.Key;
+        console.log("First Endpoint Data:", locationData);
+
         // Fetch data from the second endpoint
-        return fetch(`${baseUrl}forecasts/v1/daily/1day/${locationKey}?apikey=${apiKey}&language=pt-br&details=true`);
+        return fetch(
+          `${baseUrl}currentconditions/v1/${locationKey}?apikey=${apiKey}`
+        );
       })
-      .then(response => response.json())
-      .then(forecastData => {
-        console.log('Third Endpoint Data:', forecastData);
-        // Merge data from the second endpoint into weatherData
-        setWeatherData(prevData => ({ ...prevData, ...forecastData[0] }));
+      .then((response) => response.json())
+      .then((data) => {
+        currentConditionsData = data; // Assign data to currentConditionsData
+        console.log("Second Endpoint Data:", currentConditionsData);
+
+        // Fetch data from the third endpoint
+        return fetch(
+          `${baseUrl}forecasts/v1/daily/1day/${locationKey}?apikey=${apiKey}&language=pt-br&details=true`
+        );
       })
-      .catch(error => {
-        console.error('Error fetching weather:', error);
+      .then((response) => response.json())
+      .then((forecastData) => {
+        console.log("Third Endpoint Data:", forecastData);
+
+        // Merge data from the third endpoint into weatherData
+        setWeatherData({
+          locationData,
+          currentConditionsData: currentConditionsData[0],
+          forecastData: forecastData[0],
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching weather:", error);
       });
-        };
+  };
 
   const isDayTime = weatherData?.IsDayTime || false;
-  const dayAndMonth = weatherData ? getFormattedDate(weatherData.LocalObservationDateTime) : '';
+  const dayAndMonth = weatherData
+    ? getFormattedDate(
+        weatherData.currentConditionsData.LocalObservationDateTime
+      )
+    : "";
 
   return (
     <View style={styles.container}>
       <View style={styles.pesquisa}>
         <TextInput
           style={styles.input}
-          onChangeText={text => setCity(text)}
+          onChangeText={(text) => setCity(text)}
           value={city}
           placeholder="Enter city name"
         />
@@ -71,55 +92,78 @@ export default function App() {
       <View style={styles.hello}>
         {weatherData && (
           <>
-            <Text style={styles.hi}>Hi, {weatherData.EnglishName}</Text>
-            <Text style={styles.have}>{isDayTime ? 'Have a good day' : 'Have a good night'}</Text>
+            <Text style={styles.hi}>
+              Hi, {weatherData.locationData.EnglishName},{" "}
+              {weatherData.locationData.Country.LocalizedName}
+            </Text>
+            <Text style={styles.have}>
+              {isDayTime ? "Have a good day" : "Have a good night"}
+            </Text>
           </>
         )}
       </View>
       <View style={styles.weatherContainer}>
-        {weatherData &&(
+        {weatherData && (
           <>
-          <Image source={sun} style={styles.weatherIcons} />
-          <Text style={styles.tempText}>{weatherData.Temperature.Metric.Value}C°</Text>
-          <Text style={styles.weatherText}>{weatherData.WeatherText}</Text>
+            <Image source={sun} style={styles.weatherIcons} />
+            <Text style={styles.tempText}>
+              {weatherData.currentConditionsData.Temperature.Metric.Value}C°
+            </Text>
+            <Text style={styles.weatherText}>
+              {weatherData.currentConditionsData.WeatherText}
+            </Text>
           </>
         )}
-        {weatherData && <Text style={styles.weatherText}>Day {dayAndMonth}</Text>}
+        {weatherData && (
+          <Text style={styles.weatherText}>Day {dayAndMonth}</Text>
+        )}
       </View>
-      
+      <View style={styles.infos}>
+        <View>{weatherData && <Text>Sunrise {weatherData.Sun}</Text>}</View>
+        <View>{weatherData && <Text>Sunset{weatherData.Sun}</Text>}</View>
+      </View>
+      <View style={styles.infos}>
+        <View>
+          {weatherData && <Text>Air Quality{weatherData.Category}</Text>}
+        </View>
+        <View>{weatherData && <Text>Humidity{weatherData.Sun}</Text>}</View>
+      </View>
+      <View style={styles.infos}>
+        <View>{weatherData && <Text>Preciptation{weatherData.Sun}</Text>}</View>
+        <View>{weatherData && <Text>CLIMA %{weatherData.Sun}</Text>}</View>
+      </View>
     </View>
   );
 }
 
+// {isDayTime ? `weatherData.forecastData.Sun.Set` : `weatherData.forecastData.Moon.Set`}
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
 
   pesquisa: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    justifyContent: "flex-start",
     paddingTop: 50,
     paddingLeft: 30,
   },
   input: {
     height: 40,
     width: 220,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 20,
     borderWidth: 1,
     marginBottom: 10,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   hi: {
-    fontSize: 17
+    fontSize: 17,
   },
   have: {
-    fontSize: 25
+    fontSize: 25,
   },
   weatherContainer: {
     marginTop: 50,
-    alignItems: 'center',
+    alignItems: "center",
   },
   tempText: {
     marginTop: 15,
@@ -132,14 +176,14 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     width: 80,
     height: 40,
-    backgroundColor: '#453264',
+    backgroundColor: "#453264",
     borderRadius: 45,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 16,
-    color: '#FBEAFF',
+    color: "#FBEAFF",
   },
   hello: {
     paddingTop: 20,
@@ -147,6 +191,11 @@ const styles = StyleSheet.create({
   },
   weatherIcons: {
     width: 275,
-    height: 275
+    height: 275,
+  },
+  infos: {
+    marginTop: 30,
+    justifyContent: "space-around",
+    flexDirection: "row",
   },
 });
